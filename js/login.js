70d7c2a624432b84d8433852d7dade604dc4a256
unknown <carlosgabrield985@gmail.com>
2020-12-01 22:46:54 -0300

    + login quase completo

---
+++ js/login.js
@@ -0,0 +1,25 @@
+let forgot = document.getElementById("forgotten_password");
+
+forgot.addEventListener("click", ()=>{
+    let corpo = document.querySelector(".formulario")
+    corpo.innerHTML = `<body>
+    <header></header>
+    <form id = "recover_password">
+        <h2 id = "title">Password recovery</h2>
+        <h4 id = "#"> Enter the registered email below to receive password recovery instructions.</h4>
+        <input id = "login_input" type = "text">
+        <button onclick= "abrirModal()" id = "btn" data-toggle="modal" data-target="#exibir-mensagem"> Send </button>
+    </form> 
+    <footer></footer>
+    <script src ="recuperar_senha.js"></script>
+    </body>`
+});
+
+function abrirModal (){
+    let tituloModal = document.querySelector(".modal-title");
+    tituloModal.innerHTML = `<img id="logoExibicao" src="../img/logoResilia2.png">
+                              `;
+    let corpoModal = document.querySelector(".modal-body");
+    corpoModal.innerText = `Email successfully sent!`;
+};
+
